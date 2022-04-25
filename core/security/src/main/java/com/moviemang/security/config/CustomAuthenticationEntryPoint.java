a2897b5272847cae3295312f99eef186df7cbad4
joohy <joohy90@gmail.com>
2022-04-26 01:24:40 +0900

    ..test

--- core/security/src/main/java/com/moviemang/security/config/CustomAuthenticationEntryPoint.java
+++ core/security/src/main/java/com/moviemang/security/config/CustomAuthenticationEntryPoint.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         CommonResponse commonResponse;
