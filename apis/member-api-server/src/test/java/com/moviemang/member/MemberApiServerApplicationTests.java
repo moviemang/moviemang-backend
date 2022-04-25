2063fd4632a4d13111ef8f9fc9ad0ffd262536af
beetrbgus <devbeetrb0214@gmail.com>
2022-04-25 23:38:17 +0900

    test - 회원 가입 테스트 - 이메일 체크 테스트 추가

--- apis/member-api-server/src/test/java/com/moviemang/member/MemberApiServerApplicationTests.java
+++ apis/member-api-server/src/test/java/com/moviemang/member/MemberApiServerApplicationTests.java
@@ -1,13 +1,41 @@
 package com.moviemang.member;
 
+import com.moviemang.datastore.entity.maria.Member;
+import com.moviemang.member.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Slf4j
+@EnableJpaAuditing
 @SpringBootTest
 class MemberApiServerApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    void registMember() {
+        Member member = Member.builder()
+                .memberEmail("testuser45@gmail.com")
+                .memberName("쿠1쿠2쿠3")
+                .memberPassword("testpassword1354")
+                .build();
+        memberService.regist(member);
+        log.info("member :  "+member.toString());
+    }
+    @Test
+    void checkEmail() {
+        String email = "testuser5@gmail.com";
+        boolean isDuplicated =memberService.checkEmail(email);
+
+        log.info("member :  "+isDuplicated);
     }
 
 }
