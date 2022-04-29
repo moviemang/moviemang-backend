d3222938d2fface454ffbbfb2a89c0e5931ffefc
beetrbgus <devbeetrb0214@gmail.com>
2022-04-29 20:32:49 +0900

    feat - 이메일 자바 엔티티 생성 - 가입 Dto 생성

---
+++ core/datastore/src/main/java/com/moviemang/datastore/dto/member/MemberJoinDto.java
@@ -0,0 +1,46 @@
+package com.moviemang.datastore.dto.member;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.moviemang.datastore.entity.maria.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Table(name = "member")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberJoinDto extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("member_id")
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Email(message = "이메일을 올바르게 입력하세요.")
+    @JsonProperty("member_email")
+    @Column(name = "member_email")
+    private String memberEmail; // 이메일
+
+    @NotBlank
+    @JsonProperty("member_name")
+    @Column(name = "member_name")
+    private String memberName; // 닉네임
+
+    @NotBlank
+    @JsonProperty("member_password")
+    @Column(name = "member_password")
+    private String memberPassword; // 비밀번호
+
+    @Builder
+    public MemberJoinDto(Long memberId, String memberEmail, String memberName, String memberPassword, String memberType) {
+        this.memberId = memberId;
+        this.memberEmail = memberEmail;
+        this.memberPassword = memberPassword;
+        this.memberName = memberName;
+    }
+
+}
---
+++ core/datastore/src/main/java/com/moviemang/datastore/entity/maria/MailServiceUser.java
@@ -0,0 +1,33 @@
+package com.moviemang.datastore.entity.maria;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Data
+@Table(name = "mail_service_user")
+public class MailServiceUser extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mail_service_id")
+    private Long mailServiceId;
+    @Column(name = "member_id")
+    private Long memberId;
+    @Column(name = "mebmer_email")
+    private String memberEmail;
+    @Column(name = "content_type")
+    private String contentType;
+
+    @Builder
+    public MailServiceUser(Long mailServiceId,Long memberId, String memberEmail, String contentType) {
+        this.mailServiceId = mailServiceId;
+        this.memberId = memberId;
+        this.memberEmail = memberEmail;
+        this.contentType = contentType;
+    }
+}
