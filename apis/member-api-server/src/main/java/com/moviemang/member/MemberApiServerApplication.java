a5c04b869e23e6002ed2d2424854f4b2aa563689
kdj9878 <kdj9878@gmail.com>
2022-05-01 01:26:19 +0900

    #feat
    - BaseTimeEntity reg_date, mod_date 시간 자동 생성 어노테이션 추가

--- apis/member-api-server/src/main/java/com/moviemang/member/MemberApiServerApplication.java
+++ apis/member-api-server/src/main/java/com/moviemang/member/MemberApiServerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @ComponentScan(basePackages = "com.moviemang")
+@EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MemberApiServerApplication {
 
