5a5959f7370776ddad35caf4ae24e93428cd7288
beetrbgus <devbeetrb0214@gmail.com>
2022-05-01 22:46:10 +0900

    MemberApiServerApplication에 @Audit 어노테이션 추가

--- apis/member-api-server/src/main/java/com/moviemang/member/MemberApiServerApplication.java
+++ apis/member-api-server/src/main/java/com/moviemang/member/MemberApiServerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableJpaAuditing
 @ComponentScan(basePackages = "com.moviemang")
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MemberApiServerApplication {
