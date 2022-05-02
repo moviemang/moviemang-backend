d7d124724403be2c34a7d716657f4d868655dd7a
kdj9878 <kdj9878@gmail.com>
2022-05-03 01:02:11 +0900

    #fix
    - Auto Increment 옵션 설정
    - BaseTimeEntity 사용

--- core/datastore/src/main/java/com/moviemang/datastore/entity/maria/LoginLog.java
+++ core/datastore/src/main/java/com/moviemang/datastore/entity/maria/LoginLog.java
@@ -1,29 +1,19 @@
 package com.moviemang.datastore.entity.maria;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDateTime;
 
 @Entity
-public class LoginLog {
+public class LoginLog extends BaseTimeEntity{
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long seq;
     private Long memberId;
     private String ip;
     private String device;
 
-    @CreatedDate
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Column(name = "reg_date")
-    @JsonProperty("reg_date")
-    private LocalDateTime regDate; // 회원 등록일
+
 
 }
