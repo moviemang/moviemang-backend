a459c976bda9a42ee94f27431dbd050533f0c103
beetrbgus <devbeetrb0214@gmail.com>
2022-05-05 13:37:30 +0900

    refactor - 중복체크 RequestBody로 변경 - 에러코드 수정 - Spring security 인증 예외 중복체크 추가

--- apis/member-api-server/src/main/java/com/moviemang/member/controller/MemberController.java
+++ apis/member-api-server/src/main/java/com/moviemang/member/controller/MemberController.java
@@ -9,10 +9,18 @@ import com.moviemang.member.service.MemberService;
 import com.moviemang.security.uitls.AuthenticationUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Slf4j
 @CrossOrigin(origins = "*")
@@ -33,14 +41,14 @@ public class MemberController {
     public CommonResponse postRegist(@RequestBody @Validated MemberJoinDto memberJoinDto){
         return memberService.regist(memberJoinDto);
     }
-    @GetMapping(path = "/emailcheck/{email}")
-    public CommonResponse getEmailCheck(@PathVariable(value = "email") String email){
-        return memberService.checkEmail(email);
+
+    @PostMapping(path = "/emailcheck")
+    public CommonResponse getEmailCheck(@RequestBody Map<String,String> email){
+        return memberService.checkEmail(email.get("email"));
     }
-    @GetMapping(path = "/nickcheck/{nick}")
-    public CommonResponse getNickCheck(@PathVariable String nick)
-    {
-        return memberService.checkNick(nick);
+    @PostMapping(path = "/nickcheck")
+    public CommonResponse getNickCheck(@RequestBody Map<String,String> nick){
+        return memberService.checkNick(nick.get("nick"));
     }
 
     @DeleteMapping
@@ -50,6 +58,7 @@ public class MemberController {
     }
 	@PostMapping(path = "/certificationCheck")
     public CommonResponse checkCertification(@RequestBody MailCertificationDto certificationDto){
+    	log.info("certificationDto : {}", certificationDto.toString());
     	return memberService.checkMailCertification(certificationDto);
     }
 
