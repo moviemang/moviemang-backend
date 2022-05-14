e1f4faa7ee2a6cbbb44005223264ecdfd2ddd3ae
kdj9878 <kdj9878@gmail.com>
2022-05-14 22:33:50 +0900

    #Refactor
    - 플레이리스트 컬렉션에 이미지 경로 저장으로 인한 코드 수정

--- core/datastore/src/main/java/com/moviemang/datastore/domain/PlaylistLikeJoin.java
+++ core/datastore/src/main/java/com/moviemang/datastore/domain/PlaylistLikeJoin.java
@@ -1,6 +1,7 @@
 package com.moviemang.datastore.domain;
 
 import com.moviemang.datastore.entity.mongo.Like;
+import com.moviemang.datastore.entity.mongo.MovieInfo;
 import com.moviemang.datastore.entity.mongo.Tag;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -20,18 +21,17 @@ public class PlaylistLikeJoin {
     private String playlistTitle;
     private Long memberId;
     private List<Tag> tags;
-    private List<Integer> movieIds;
+    private List<MovieInfo> movies;
     private boolean display;
     private List<Like> likes;
 
     @Builder
-    public PlaylistLikeJoin(ObjectId _id, String playlistTitle, Long memberId
-            , List<Tag> tags, List<Integer> movieIds, boolean display, List<Like> likes) {
+    public PlaylistLikeJoin(ObjectId _id, String playlistTitle, Long memberId, List<Tag> tags, List<MovieInfo> movies, boolean display, List<Like> likes) {
         this._id = _id;
         this.playlistTitle = playlistTitle;
         this.memberId = memberId;
         this.tags = tags;
-        this.movieIds = movieIds;
+        this.movies = movies;
         this.display = display;
         this.likes = likes;
     }
