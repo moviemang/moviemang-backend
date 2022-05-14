fcf0fd33fbae680351b3c651ce95ed3ec4ba3dd4
beetrbgus <88869529+beetrbgus@users.noreply.github.com>
2022-05-14 19:35:07 +0900

    Revert "Revert "Revert "Revert "#Refactor""""

--- core/datastore/src/main/java/com/moviemang/datastore/repository/mongo/playlist/CustomizedPlaylistRepository.java
+++ core/datastore/src/main/java/com/moviemang/datastore/repository/mongo/playlist/CustomizedPlaylistRepository.java
@@ -1,4 +1,10 @@
 package com.moviemang.datastore.repository.mongo.playlist;
 
+import com.moviemang.datastore.domain.PlaylistLikeJoin;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+
 public interface CustomizedPlaylistRepository {
+
+    AggregationResults<PlaylistLikeJoin> playlistOrderByLike(Aggregation aggregation, String collection);
 }
