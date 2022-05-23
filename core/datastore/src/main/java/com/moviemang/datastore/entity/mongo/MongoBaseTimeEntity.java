99d2f430e8635f11d497a187ec218b4a49ecda62
kdj9878 <kdj9878@gmail.com>
2022-05-23 20:19:47 +0900

    #style
    - 불필요한 코드 제거

--- core/datastore/src/main/java/com/moviemang/datastore/entity/mongo/MongoBaseTimeEntity.java
+++ core/datastore/src/main/java/com/moviemang/datastore/entity/mongo/MongoBaseTimeEntity.java
@@ -1,6 +1,5 @@
 package com.moviemang.datastore.entity.mongo;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -16,7 +15,6 @@ public abstract class MongoBaseTimeEntity {
 
     @CreatedDate
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime regDate;
 
     @LastModifiedDate
